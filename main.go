41aeef3e044e3abd354c8fec168e9f10c60c45ee
Krishnanunni333 <kittuvirus65@gmail.com>
2024-04-20 20:27:42 +0530

    created the boilerplate code and basic server logic

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	router := chi.NewRouter()
+	router.Use(
+		cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300,
+		}))
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/error", handlerError)
+	
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	log.Printf("Starting server on port " + port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
